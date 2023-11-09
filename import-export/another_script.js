e19c90118382b0a4b1eb414a787f4b940dc40d02
perica <petar.kolic@icentic.com>
2023-11-09 18:35:18 +0100

    javascript dodavanje

---
+++ import-export/another_script.js
@@ -0,0 +1,2 @@
+export var varijabla1 = "gumica"
+export var varijabla2 = "olovka"
---
+++ import-export/scripts.js
@@ -0,0 +1,3 @@
+import * as klasa1 from './another_script.js'
+
+document.getElementById("test1").innerHTML = klasa1.varijabla1 + " " + klasa1.varijabla2 ;
